41c32bb80f980369602ab978becd649d4111c10e
Thomas Kelly <thomask540@gmail.com>
2022-05-25 11:01:06 +0100

    fix json decode

--- test/test_decode.py
+++ test/test_decode.py
@@ -13,11 +13,8 @@ class testDecode(unittest.TestCase):
 
     def test_decode_json(self):
         json_to_decode = 'eyJoZWxsbyI6ICJ3b3JsZCJ9'
-        expected_result = json.dumps({"hello": "world"}, indent=4, sort_keys=True)
+        expected_result = json.dumps({"hello": "world"}, indent=4, sort_keys=True).encode()
 
         decoded_json = decode_json(json_to_decode)
 
         self.assertEqual(decoded_json, expected_result)
-
-
-        {"hello": "world"}
